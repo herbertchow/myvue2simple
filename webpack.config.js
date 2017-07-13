12bb0f18c731605faeda00a61d65cc382827832d
netease_wb1 <495567615@qq.com>
2017-07-13 16:01:22 +0800

    path

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/dist/',
+    publicPath: '/myvue2simple/dist/',
     filename: 'build.js'
   },
   module: {
